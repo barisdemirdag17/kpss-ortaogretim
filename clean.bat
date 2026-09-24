a640e6636ee1bd470da001d0c69585e04c4d6624
KPSS Rehber <deploy@kpss-ortaogretim.com>
2026-09-25 01:31:59 +0300

    Remove bat files

---
+++ clean.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\Program Files\Git\bin\git.exe" -C "C:/Users/baris/OneDrive/Desktop/opencode otomasyon/kpss-ortaogretim-website" rm commit.bat push-git.bat 2>nul
+"C:\Program Files\Git\bin\git.exe" -C "C:/Users/baris/OneDrive/Desktop/opencode otomasyon/kpss-ortaogretim-website" add -A
+"C:\Program Files\Git\bin\git.exe" -C "C:/Users/baris/OneDrive/Desktop/opencode otomasyon/kpss-ortaogretim-website" commit -m "Remove bat files"
+DONE
